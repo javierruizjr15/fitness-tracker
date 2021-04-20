3f8d6f3256c7b3ff80c4e5a3ae5d24de3b09a447
Javier Ruiz <javierruizjr15@gmail.com>
2021-04-20 10:02:36 -0700

    fixed

---
+++ routes/workoutRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router()
+const { Workout } = require('../models')
+
+router.get('/workout', (req, res) => {
+  Workout.findById()
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+router.get('/workouts/range', (req, res) => {
+  Workout.find().limit(7)
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+router.get('/workouts', (req, res) => {
+  Workout.find()
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+router.post('/workouts', (req, res) => {
+  Workout.create({})
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+router.put('/workouts/:id', (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+router.delete('/workouts/:id', (req, res) => {
+  Workout.findByIdAndDelete(req.params.id)
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+
+module.exports = router
+
